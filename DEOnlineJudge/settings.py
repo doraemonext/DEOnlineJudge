3921147184510fb3f6331e485cb3f9f04ff2e8cc
doraemonext <doraemonext@gmail.com>
2015-07-12 20:08:10 +0800

    Fix: 分离主页内容

--- DEOnlineJudge/settings.py
+++ DEOnlineJudge/settings.py
@@ -60,10 +60,13 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'DEOnlineJudge.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
