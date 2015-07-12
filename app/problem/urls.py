ae0f8c9e84607da43c23711697f385a54e001088
doraemonext <doraemonext@gmail.com>
2015-07-12 21:25:15 +0800

    Add: 添加题目详细信息页

--- app/problem/urls.py
+++ app/problem/urls.py
@@ -2,8 +2,9 @@
 
 from django.conf.urls import include, url
 
-from app.problem.views import ProblemListView
+from app.problem.views import ProblemListView, ProblemDetailView
 
 urlpatterns = [
     url(r'^list/$', ProblemListView.as_view(), name='list'),
+    url(r'^detail/(?P<id>[0-9]+)/$', ProblemDetailView.as_view(), name='detail'),
 ]
--- app/problem/views.py
+++ app/problem/views.py
@@ -6,3 +6,12 @@ from django.views.generic import TemplateView
 
 class ProblemListView(TemplateView):
     template_name = 'problem/list.html'
+
+
+class ProblemDetailView(TemplateView):
+    template_name = 'problem/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProblemDetailView, self).get_context_data(**kwargs)
+        context['title'] = 'This is a Program'
+        return context
