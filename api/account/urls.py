07229619516cac8e7704094bcef4314865b2e56f
doraemonext <doraemonext@gmail.com>
2015-07-13 07:53:19 +0800

    Fix: 修正登录URL

--- api/account/urls.py
+++ api/account/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from api.account.views import RegistrationAPI, LoginAPI, LogoutAPI
 
 urlpatterns = [
-    url(r'^login/$', RegistrationAPI.as_view(), name='registration'),
+    url(r'^registration/$', RegistrationAPI.as_view(), name='registration'),
     url(r'^login/$', LoginAPI.as_view(), name='login'),
     url(r'^logout/$', LogoutAPI.as_view(), name='logout'),
 ]
