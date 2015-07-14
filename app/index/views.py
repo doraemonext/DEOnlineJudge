648845daf457c72b469e7f3a1aeb2cf8f3a6321e
doraemonext <doraemonext@gmail.com>
2015-07-14 18:20:48 +0800

    Fix: 删除首页

--- app/index/views.py
+++ app/index/views.py
@@ -3,13 +3,8 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.core.urlresolvers import reverse
-from django.views.generic import TemplateView, View
+from django.views.generic import View
 from django.http.response import HttpResponseRedirect
-from django.db.models import Count
-
-from app.record.models import Record
-from app.problem.models import Problem
-from system.users.models import User
 
 
 class IndexView(View):
