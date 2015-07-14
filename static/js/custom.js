cf57be093421070995113ce2d6d1b9c5c6e04493
doraemonext <doraemonext@gmail.com>
2015-07-14 20:00:30 +0800

    Fix: 修复首页刷新问题

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,42 +1,8 @@
 $(document).ready(function(){
 	
-	// dropdown in leftmenu
-	$('.leftmenu .dropdown > a').click(function(){
-		if(!$(this).next().is(':visible'))
-			$(this).next().slideDown('fast');
-		else
-			$(this).next().slideUp('fast');	
-		return false;
-	});
-	
-	
-	if($.uniform) 
+	if($.uniform)
 	   $('input:checkbox, input:radio, .uniform-file').uniform();
-		
-	if($('.widgettitle .close').length > 0) {
-		  $('.widgettitle .close').click(function(){
-					 $(this).parents('.widgetbox').fadeOut(function(){
-								$(this).remove();
-					 });
-		  });
-	}
-	
-	
-	//// show/hide left menu
-	//$(window).resize(function () {
-	//	if ($('.topbar').is(':visible')) {
-	//		if ($('.barmenu').hasClass('open')) {
-	//			$('.rightpanel, .headerinner').css({marginLeft: '260px'});
-	//			$('.logo, .leftpanel').css({marginLeft: 0});
-	//		} else {
-	//			$('.rightpanel, .headerinner').css({marginLeft: 0});
-	//			$('.logo, .leftpanel').css({marginLeft: '-260px'});
-	//		}
-	//	} else {
-	//		$('.rightpanel, .headerinner').css({marginLeft: '260px'});
-	//		$('.logo, .leftpanel').css({marginLeft: 0});
-	//	}
-	//});
+
 	
 	// dropdown menu for profile image
 	$('.userloggedinfo img').click(function(){
@@ -49,84 +15,6 @@ $(document).ready(function(){
 					 }
 		  }
    });
-	
-	// change skin color
-	$('.skin-color a').click(function(){ return false; });
-	$('.skin-color a').hover(function(){
-		var s = $(this).attr('href');
-		if($('#skinstyle').length > 0) {
-			if(s!='default') {
-				$('#skinstyle').attr('href','css/style.'+s+'.css');	
-				$.cookie('skin-color', s, { path: '/' });
-			} else {
-				$('#skinstyle').remove();
-				$.cookie("skin-color", '', { path: '/' });
-			}
-		} else {
-			if(s!='default') {
-				$('head').append('<link id="skinstyle" rel="stylesheet" href="css/style.'+s+'.css" type="text/css" />');
-				$.cookie("skin-color", s, { path: '/' });
-			}
-		}
-		return false;
-	});
-	
-	// load selected skin color from cookie
-	if($.cookie('skin-color')) {
-		var c = $.cookie('skin-color');
-		if(c) {
-			$('head').append('<link id="skinstyle" rel="stylesheet" href="css/style.'+c+'.css" type="text/css" />');
-			$.cookie("skin-color", c, { path: '/' });
-		}
-	}
-	
-	
-	// expand/collapse boxes
-	if($('.minimize').length > 0) {
-		  
-		  $('.minimize').click(function(){
-					 if(!$(this).hasClass('collapsed')) {
-								$(this).addClass('collapsed');
-								$(this).html("&#43;");
-								$(this).parents('.widgetbox')
-										      .css({marginBottom: '20px'})
-												.find('.widgetcontent')
-												.hide();
-					 } else {
-								$(this).removeClass('collapsed');
-								$(this).html("&#8211;");
-								$(this).parents('.widgetbox')
-										      .css({marginBottom: '0'})
-												.find('.widgetcontent')
-												.show();
-					 }
-					 return false;
-		  });
-			  
-	}
-	
-	// fixed right panel
-	var winSize = $(window).height();
-	if($('.rightpanel').height() < winSize) {
-		$('.rightpanel').height(winSize);
-	}
-	
-	
-	// if facebook like chat is enabled
-	if($.cookie('enable-chat')) {
-		
-		$('body').addClass('chatenabled');
-		$.get('ajax/chat.html',function(data){
-			$('body').append(data);
-		});
-		
-	} else {
-		
-		if($('.chatmenu').length > 0) {
-			$('.chatmenu').remove();
-		}
-		
-	}
 
     // 设置CSRF
 	$.getCookie = function getCookie(name) {
